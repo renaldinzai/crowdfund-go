5ee61511a074e77f81cd17f3d49ef5abbb103920
renaldin <renaldinzai@inovasi360.com>
2022-08-11 00:26:24 +0700

    Initialize project, add new features for adding new user

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crowdfund-go/handler"
+	"crowdfund-go/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:mysql@tcp(localhost:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
